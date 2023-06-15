f1565827e26cc522429ee257a446717945adc052
Zynakill <132162958+Zynakill@users.noreply.github.com>
2023-06-15 23:52:07 +0400

    Добавлена команда /mines

--- modules/economy.py
+++ modules/economy.py
@@ -120,18 +120,18 @@ class EconomyCog(commands.Cog):
             r = frange(1, 10, .1)
         t = random.choice(r)
         embed = disnake.Embed(
-            title=locales.get("CASINO").capitalize(),
+            title=f"{locales.get('CASINO').capitalize()} — {inter.author.display_name.capitalize()}",
             colour=0x2b2d31,
             description=locales.get("CASINO_EMB_DESCR")
         )
         embed.set_thumbnail(url=inter.author.display_avatar.url)
-        await inter.send(embed=embed, components=[disnake.ui.Button(label=locales.get("STOP"))])
+        await inter.send(embed=embed, components=[disnake.ui.Button(label=locales.get("STOP"), custom_id="casino_stop")])
         self.casino[inter.author.id] = [inter.guild.id, True]
         for i in r:
             if self.casino[inter.author.id][0] == inter.guild.id and self.casino[inter.author.id][1]:
                 embed.clear_fields()
                 embed.add_field(
-                    name=locales.get("CASINO_NAME"),
+                    name=locales.get('CASINO_NAME'),
                     value=f"```{i}```"
                 )
                 if i == t:
@@ -190,15 +190,139 @@ class EconomyCog(commands.Cog):
         db.users.update_one({"gid": inter.guild.id, "id": member.id}, {"$inc": {"money": money}})
         db.users.update_one({"gid": inter.guild.id, "id": inter.author.id}, {"$inc": {"money": -money}})
         embed = disnake.Embed(
-            title=locales.get("PAY").capitalize(),
+            title=f"{locales.get('PAY').capitalize()} — {inter.author.display_name.capitalize()}",
             description=locales.get("PAY_EMB").replace("{dollars}", str(money)).replace("{member}", member.display_name.capitalize())
         )
         await inter.send(embed=embed)
 
+    @commands.slash_command(
+        name=disnake.Localized(key="MINES"),
+        description=disnake.Localized(key="MINES_DESCRIPTION")
+    )
+    async def mines(self, inter: disnake.ApplicationCommandInteraction, bet: int = commands.Param(
+        name=disnake.Localized(key="BET"),
+        description=disnake.Localized(key="BET_DESCR")
+    )):
+        await inter.response.defer()
+        u = db.get_user(inter.author)
+        locales = i18n.Init(inter)
+        if u["fires"] < bet:
+            return await inter.send(embed=i18n.no_fires_emb(locales, inter.author))
+        db.users.update_one({"gid": inter.guild.id, "id": inter.author.id}, {"$inc": {"fires": -bet}})
+        boom = []
+
+        def add():
+            v = (random.randint(0, 2), random.randint(0, 2))
+            if v not in boom:
+                boom.append(v)
+            else:
+                return add()
+        for _ in range(3):
+            add()
+
+        buttons = [
+            [disnake.ui.Button(emoji="💣", custom_id=f"mine_{line}_{row}_{boom}_{bet}") for row in range(3)] for line in
+            range(3)
+        ]
+        embed = disnake.Embed(
+            title=f"{locales.get('MINES').capitalize()} — {inter.author.display_name.capitalize()}",
+            colour=0x2b2d31,
+            description=locales.get("MINES_EMB_DESCRIPTION")
+        )
+        embed.set_thumbnail(url=inter.author.display_avatar.url)
+        await inter.send(embed=embed, components=buttons)
+
     @commands.Cog.listener()
     async def on_button_click(self, inter: disnake.MessageInteraction):
-        if inter.component.label == "Стоп":
+        if not inter.author.id == inter.user.id:
+            return
+        locales = i18n.Init(inter)
+        if inter.component.custom_id == "casino_stop":
             self.casino[inter.author.id][1] = False
+        elif inter.component.custom_id.startswith("mine"):
+            await inter.response.defer()
+            s = inter.component.custom_id.split("_")
+            boom = eval(s[3])  # [(line, row) x3]
+            pos = (int(s[1]), int(s[2]))  # line, row
+            bet = s[4]
+            buttons = []
+            b = False
+            checked = False
+
+            def check():
+                nonlocal b, checked
+                for line in range(3):
+                    buttons.append([])
+                    for n, row in enumerate(inter.message.components[line].children, start=0):
+                        custom_id = f"mine_{line}_{n}_{boom}_{bet}"
+                        emoji = "💣"
+                        disabled = True if custom_id == inter.component.custom_id else False
+                        this_pos = (line, n)
+                        if this_pos in boom and disabled:
+                            emoji = "🔥"
+                            b = True
+                            if not checked:
+                                checked = True
+                                buttons.clear()
+                                return check()
+                        if b:
+                            if this_pos in boom:
+                                emoji = "🔥"
+                            disabled = True
+                        if row.disabled:
+                            emoji = "💣"
+                            disabled = True
+                        buttons[line].append(disnake.ui.Button(
+                            emoji=emoji,
+                            custom_id=custom_id,
+                            disabled=disabled
+                        ))
+            check()
+            embed = inter.message.embeds[0]
+            if b:
+                embed.description = locales.get("MINES_LOST_EMB")
+                embed.add_field(
+                    name=f"> {locales.get('BET').capitalize()}:",
+                    value=f"```{bet}```"
+                )
+            else:
+                buttons.append([disnake.ui.Button(label=locales.get("STOP"), custom_id="m_stop")])
+
+            await inter.edit_original_message(embed=embed, components=buttons)
+        elif inter.component.custom_id == "m_stop":
+            await inter.response.defer()
+            x = 0
+            buttons = []
+            bet = 0
+            for i in range(3):
+                buttons.append([])
+                for n, btn in enumerate(inter.message.components[i].children, start=0):
+                    pos = (i, n)
+                    boom = eval(btn.custom_id.split("_")[3])
+                    bet = int(btn.custom_id.split("_")[4])
+                    if pos in boom:
+                        btn.emoji = "🔥"
+                    if btn.disabled:
+                        x += 1.3
+                    btn.disabled = True
+                    buttons[i].append(disnake.ui.Button.from_component(btn))
+            embed = inter.message.embeds[0]
+            embed.description = locales.get("MINES_WIN_EMB")
+            embed.add_field(
+                name=f"> {locales.get('MINES_WIN')}:",
+                value=f"```{round(bet*x)}```",
+                inline=False
+            )
+            embed.add_field(
+                name=f"> {locales.get('BET').capitalize()}:",
+                value=f"```{bet}```"
+            )
+            embed.add_field(
+                name=locales.get('CASINO_COF'),
+                value=f"```{x}```"
+            )
+            db.users.update_one({"gid": inter.guild.id, "id": inter.author.id}, {"$inc": {"fires": round(bet*x)}})
+            await inter.edit_original_message(embed=embed, components=buttons)
 
 
 def setup(bot):
